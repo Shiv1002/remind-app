9553de20f17a0ddd834680da889980da6b5fc9bc
shivank <shivank.pasi@somaiya.edu>
2023-05-02 21:10:39 +0530

    toast feature added

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fire
 import axios from 'axios';
 
 function App() {
-  const [user,setUser] = useState({uid:localStorage.getItem("userUid"),email:localStorage.getItem("userEmail")})
+  const [user, setUser] = useState({ uid: localStorage.getItem("userUid"), email: localStorage.getItem("userEmail") })
   const [isLogged, setIsLogged] = useState(false)
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
-  useEffect(()=>{
-    if (localStorage.getItem("userUid")){
+  const [toast,setToast] = useState("");
+  useEffect(() => {
+    if (localStorage.getItem("userUid")) {
       setIsLogged(true)
-    }else{
+    } else {
       console.log("No user found ")
     }
-  },[])
+  }, [])
 
-  const clearInput=()=>{
+  const clearInput = () => {
     setEmail("")
     setPassword("")
     setPasswordError("")
     setEmailError("")
-    
+
   }
 
   const setLogin = () => {
     setIsLogged(true)
+    showToast("logged In")
+    
   }
   const setLogout = () => {
     clearInput()
@@ -38,84 +41,95 @@ function App() {
     setUser()
     localStorage.removeItem("userUid")
     localStorage.removeItem("userEmail")
+    showToast("Logged out")
   }
-  
-  const handleLogin =()=>{
+
+  const handleLogin = () => {
     //userCredentials->user->uid
     //userCredentials->user->email
-    signInWithEmailAndPassword(auth,email,password)
-    .then((userCred)=>
-    {
-      console.log(userCred.user.uid)
-      setUser(userCred.user)
-      setLogin()
-      localStorage.setItem("userUid",userCred.user.uid)
-      localStorage.setItem("userEmail",userCred.user.email)
-    }
-  )
-    .catch((err)=>{
-      console.log(err)
-      switch(err.code){
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCred) => {
+        console.log(userCred.user.uid)
+        setUser(userCred.user)
+        setLogin()
+        localStorage.setItem("userUid", userCred.user.uid)
+        localStorage.setItem("userEmail", userCred.user.email)
+      }
+      )
+      .catch((err) => {
+        console.log(err)
+        switch (err.code) {
           case "auth/invalid-email":
             setEmailError("Invalid email")
+
             break
           case "auth/user-disabled":
             setEmailError("Permission blocked")
             break
           case "auth/user-not-found":
-            setEmailError("User not found"); 
+            setEmailError("User not found");
             break;
           case "auth/wrong-password":
             setPasswordError(err.message);
             break;
           default:
-      }
-    })
-  }
-  
-  const signUp=()=>{
-    createUserWithEmailAndPassword(auth,email,password)
-    .then((userCred)=>{ 
-      console.log('Created user',userCred.user.uid)
-      setUser(userCred.user)
-      axios.post('https://reminderserver-production.up.railway.app/user/postUser',{uid:userCred.user.uid,reminders:[],email:userCred.user.email})
-      .then((rs)=>{
-        console.log(rs)
-        localStorage.setItem("userUid",userCred.user.uid)
-        localStorage.setItem("userEmail",userCred.user.email)
+        }
       })
-      .catch((err)=>console.log(err))
-      
-      setLogin()
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
   }
-  return ( 
-    <div className="">{
-      isLogged ?
-      <>
-      <span className='d-flex justify-content-end'><button className='btn position-absolute btn-success z-3 m-2 ' onClick={setLogout}>logout</button></span>
-      
-      <Remind  User={user}/>
-      </>
-        :
-        <div className='container  my-5 p-4 rounded-4 col-xl-4 col-md-6 col-sm-8 col-xs-6  '>
-        <div className='login_page d-flex flex-column align-items-center p-2'>
-          
-          <input type='email' value={email} autoFocus required placeholder='Email' onChange={(e)=>setEmail(e.target.value)}/>
-          <span className="errorMsg">{emailError}</span>
-          <input type='password' value={password} required placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
-          {/* <button className='btn btn-success' onClick={setLogin}>login</button> */}
-          <span className="errorMsg">{passwordError}</span>
-          <button  onClick={handleLogin}>Login</button>
-          <button onClick={signUp}>Sign up</button>
-        </div>
-        </div>
-        
-    }
 
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCred) => {
+        console.log('Created user', userCred.user.uid)
+        setUser(userCred.user)
+        axios.post('https://reminderserver-production.up.railway.app/user/postUser', { uid: userCred.user.uid, reminders: [], email: userCred.user.email })
+          .then((rs) => {
+            console.log(rs)
+            localStorage.setItem("userUid", userCred.user.uid)
+            localStorage.setItem("userEmail", userCred.user.email)
+          })
+          .catch((err) => console.log(err))
+
+        setLogin()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  const showToast =(msg)=>{
+    document.getElementById('cus-toast').classList.toggle('d-none')
+    setToast(msg)
+    var Toast= setInterval(()=>{document.getElementById('cus-toast').classList.toggle('d-none')},3000)
+    setTimeout(()=>{clearInterval(Toast); setToast("")},3000)
+    
+  }
+  return (
+    <div className="root position-relative p-4 ">{
+      isLogged ?
+        <div>
+          <span className='d-flex justify-content-end'><button className='btn position-absolute btn-success btn-lg z-3 m-2 border-4' onClick={setLogout}>logout</button></span>
+
+          <Remind User={user}  showToast={showToast}/>
+        </div>
+        :
+        <div className='container   rounded-4 col-xl-4 col-md-6 col-sm-8 col-xs-6  p-3 my-4'>
+          <div className='login_page  d-flex flex-column align-items-center p-2 '>
+
+            <input type='email' value={email} autoFocus required placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+            <span className="errorMsg">{emailError}</span>
+            <input type='password' value={password} required placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+            {/* <button className='btn btn-success' onClick={setLogin}>login</button> */}
+            <span className="errorMsg">{passwordError}</span>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={signUp}>Sign up</button>
+          </div>
+        </div>
+
+    }
+    
+      <div id="cus-toast" className='cus-toast d-none position-absolute end-0 bottom-0  p-3 m-3  bg-light rounded-2  '>
+        <span className=''>{toast}</span>
+      </div>
     </div>
   );
 }
--- src/Remind.js
+++ src/Remind.js
@@ -8,92 +8,98 @@ import TimePicker from 'react-time-picker';
 import { faTrash, faClock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
-function Remind({User}) {
+function Remind({ User,showToast }) {
 
- 
+
   const [allRemind, setAllRemind] = useState([])
   const [startDate, setStartDate] = useState(new Date());
   const [time, setTime] = useState('00:00');
-   const [reminder, setReminder] = useState({ text: "", date:startDate, time: '00:00' });
+  const [reminder, setReminder] = useState({ text: "", date: startDate, time: '00:00' });
 
-  useEffect(()=>{
+  useEffect(() => {
     //start getting data of user
-    console.log(process.env.BACKURL,'is pinnged')
-    axios.post('https://reminderserver-production.up.railway.app/user/getUser',{uid:User.uid})
-    .then((res)=>{setAllRemind(res.data[0].reminders);})
-    .catch((err)=>console.log(err))
-  },[User])
+    console.log(process.env.BACKURL, 'is pinnged')
+    axios.post('https://reminderserver-production.up.railway.app/user/getUser', { uid: User.uid })
+      .then((res) => { setAllRemind(res.data[0].reminders); })
+      .catch((err) => console.log(err))
+  }, [User])
 
-  const delRemind = (index) => { 
+  const delRemind = (index) => {
     var reminderName = 0
-    setAllRemind(allRemind.filter((ele, i) => { if (i !== index){ return true }else{reminderName = ele.text; return false}}))
+    setAllRemind(allRemind.filter((ele, i) => { if (i !== index) { return true } else { reminderName = ele.text; return false } }))
     console.log(reminderName)
     //sending update requesst to database
     // sending 0 as parameter to delete reminder with namee
-    axios.post(`https://reminderserver-production.up.railway.app/user/${User.uid}/0`,{reminderName})
-    .then((rs)=>console.log(rs.data))
-    .catch((err)=>console.log(err))
+    axios.post(`https://reminderserver-production.up.railway.app/user/${User.uid}/0`, { reminderName })
+      .then((rs) => console.log(rs.data))
+      .catch((err) => console.log(err))
 
   }
-  const addRemind = () => { 
-    setAllRemind([...allRemind,  reminder])
+  const addRemind = () => {
+    if(reminder.text === ""){
+      showToast("Enter something to be remind of")
+      return 
+    }
+    setAllRemind([...allRemind, reminder])
     console.log(allRemind)
     //sending update requesst to database
     // sending 1 as parameter to add reminder
-    axios.post(`https://reminderserver-production.up.railway.app/user/${User.uid}/1`,reminder)
-    .then((rs)=>console.log(rs.data))
-    .catch((err)=>console.log(err))
+    axios.post(`https://reminderserver-production.up.railway.app/user/${User.uid}/1`, reminder)
+      .then((rs) => console.log(rs.data))
+      .catch((err) => console.log(err))
 
     setStartDate(new Date())
     setReminder({ ...reminder, text: "", date: startDate })
-    
+
   }
 
   return (
     <div className='main_page '>
-      <span>Signed In as</span>
-          <h4>{User.email}</h4>
-          <div className='list  bg-light p-2 mt-3 mx-2 rounded-2'>
-            {allRemind.length > 0 ?
-              <ul>{
-                allRemind.map((ele, index) => {
-                  return <li className='d-flex justify-content-between' key={ele.text} >
-                    <div>
-                      <h2>{ele.text}</h2>
-                      <span>{Object.values(ele.date).join("/")}</span>
-                      <span>{ele.time}</span>
-                      <FontAwesomeIcon icon={faClock} />
-                    </div>
-                    <div className='d-flex align-items-center'>
-                      <FontAwesomeIcon style={{ color: '#cc1c1c' }} icon={faTrash} size='xl' className='del' onClick={(e) => delRemind(index)} />
-                    </div>
-                  </li>
-                })
-              }
-              </ul>
-              : <h4>No Reminders...</h4>}
-          </div>
+      <span className='text-light'>Signed In as</span>
+      <h4 className='text-light'>{User.email}</h4>
+      <div className='list  p-2 mt-3 mx-2 rounded-2'>
+        {allRemind.length > 0 ?
+          <ul>{
+            allRemind.map((ele, index) => {
+              return <li className='d-flex justify-content-between' key={ele.text} >
+                <div>
+                  <h2>{ele.text}</h2>
+                  <span>{Object.values(ele.date).join("/")}</span>
+                  <span>{ele.time}</span>
+                  <FontAwesomeIcon icon={faClock} />
+                </div>
+                <div className='d-flex align-items-center'>
+                  <FontAwesomeIcon style={{ color: '#cc1c1c' }} icon={faTrash} size='xl' className='del' onClick={(e) => delRemind(index)} />
+                </div>
+              </li>
+            })
+          }
+          </ul>
+          : <div className='bg-light d-flex justify-content-center p-3 rounded-4'>
+            <h4 className=''>No Reminders Yet...</h4>
+            </div>}
+      </div>
 
-          <div className='add_reminder p-4 bg-dark m-2 rounded-4'>
-            <input type='text' className='m-2 py-2 rounded-2' autoFocus placeholder='Meeting at 10 O`clock' value={reminder.text} onChange={(e) => setReminder({ ...reminder, text: e.target.value })} />
+      <div className='add_reminder p-4 bg-dark m-2 rounded-4'>
+        <input type='text' className='m-2 py-2 rounded-2' autoFocus placeholder='Meeting at 10 O`clock' value={reminder.text} onChange={(e) => setReminder({ ...reminder, text: e.target.value })} />
 
-            <DatePicker selected={startDate} className='m-2 py-2 rounded-2' onChange={(date) => {
-              setStartDate(date)
-              setReminder({ ...reminder, date: { year: date.getFullYear(), month: date.getMonth(), day: date.getDate() } })
-              console.log(reminder)
-            }} />
-            <TimePicker className="timepicker m-2 py-2" value={time} onChange={(newTime) => {
-              setTime(newTime)
-              setReminder({ ...reminder, time: newTime })
-              console.log(time)
-            }} />
-            <div>
-              <button className='btn btn-success' onClick={() => addRemind()}>Add a reminder</button>
-            </div>
-
-          </div>
-
+        <DatePicker selected={startDate} className='m-2 py-2 rounded-2' onChange={(date) => {
+          setStartDate(date)
+          setReminder({ ...reminder, date: { year: date.getFullYear(), month: date.getMonth(), day: date.getDate() } })
+          console.log(reminder)
+        }} />
+        <TimePicker className="timepicker m-2 py-2" value={time} onChange={(newTime) => {
+          setTime(newTime)
+          setReminder({ ...reminder, time: newTime })
+          console.log(time)
+        }} />
+        <div>
+          <button className='btn btn-success' onClick={() => addRemind()}>Add a reminder</button>
         </div>
+
+      </div>
+
+    </div>
   )
 }
 
